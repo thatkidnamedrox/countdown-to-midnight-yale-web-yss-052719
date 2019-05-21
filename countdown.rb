96c5876530e9289bb62c36a1caeb031f506582c6
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-21 18:14:32 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown(int)
+  counter = int
+  while counter > 0
+    puts "#{int} SECOND(S)!"
+    counter-+1
+  end
+  return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(int)
+  counter = int
+  while counter > 0
+    puts "#{int} SECOND(S)!"
+    sleep(1)
+    counter-=1
+  end
+  return "HAPPY NEW YEAR!"
+end
+  
